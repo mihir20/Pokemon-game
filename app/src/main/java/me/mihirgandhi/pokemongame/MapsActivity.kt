9c718c895fb64878505720b5ef296f49d7bf1208
mihir20 <mihir20121997@gmail.com>
2018-05-28 17:06:35 +0530

    get player current location

--- app/src/main/java/me/mihirgandhi/pokemongame/MapsActivity.kt
+++ app/src/main/java/me/mihirgandhi/pokemongame/MapsActivity.kt
@@ -1,10 +1,15 @@
 package me.mihirgandhi.pokemongame
 
+import android.content.Context
 import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
+import android.util.Log
 import android.widget.Toast
 
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -14,11 +19,14 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import java.lang.Exception
+import kotlin.concurrent.thread
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
-    private lateinit var mMap: GoogleMap
-    private val LOACTION_PERMISSION_REQUESTCODE=120
+    private var mMap: GoogleMap? = null
+    val LOACTION_PERMISSION_REQUESTCODE = 123
+    var myLocation: Location? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,28 +51,28 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
+       /* // Add a marker in Sydney and move the camera
         val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions()
                 .position(sydney)
                 .title("You")
                 .snippet("This is your locatiom")
                 .icon(BitmapDescriptorFactory.fromResource(R.drawable.mario)))
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney,16f))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 16f))*/
     }
 
-    fun checkPermission(){
+    fun checkPermission() {
 
 
-        if(Build.VERSION.SDK_INT >= 23){
+        if (Build.VERSION.SDK_INT >= 23) {
 
-            if(ActivityCompat
+            if (ActivityCompat
                             .checkSelfPermission(
                                     this,
                                     android.Manifest.permission.ACCESS_FINE_LOCATION) !=
-                    PackageManager.PERMISSION_GRANTED){
-            requestPermissions(arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
-                    LOACTION_PERMISSION_REQUESTCODE)
+                    PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                        LOACTION_PERMISSION_REQUESTCODE)
                 return
             }
 
@@ -73,23 +81,95 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     }
 
-    fun getMyPosition(){
+    fun getMyPosition() {
+
+        Toast.makeText(this,"User location access on",Toast.LENGTH_LONG).show()
+        //TODO: Will implement later
+
+        var myLocation= MyLocationListener()
+
+        var locationManager=getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,3,3f,myLocation)
+
+
+        var myLocationUpdate = UpdateLocationThread()
+        myLocationUpdate.start()
+
 
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
 
-        when(requestCode){
+        when (requestCode) {
             LOACTION_PERMISSION_REQUESTCODE -> {
-                if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     getMyPosition()
-                }else {
-                    Toast.makeText(this,"we cannot access your locaction",Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(this, "we cannot access your locaction", Toast.LENGTH_LONG).show()
                 }
             }
         }
 
 
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    inner class MyLocationListener:LocationListener {
+
+        constructor(){
+            myLocation = Location("Start")
+            myLocation!!.latitude = 0.0
+            myLocation!!.longitude = 0.0
+        }
+
+        override fun onLocationChanged(p0: Location?) {
+            myLocation = p0
+        }
+
+        override fun onStatusChanged(p0: String?, p1: Int, p2: Bundle?) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun onProviderEnabled(p0: String?) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun onProviderDisabled(p0: String?) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+    }
+
+    inner class UpdateLocationThread:Thread{
+
+        constructor():super(){
+
+        }
+
+        override fun run() {
+
+            while (true){
+                try {
+
+                    runOnUiThread {
+                        mMap!!.clear()
+                        // Add a marker on player and move the camera
+                        val playerLocation = LatLng(myLocation!!.latitude, myLocation!!.longitude)
+                        mMap!!.addMarker(MarkerOptions()
+                                .position(playerLocation)
+                                .title("You")
+                                .snippet("This is your locatiom")
+                                .icon(BitmapDescriptorFactory.fromResource(R.drawable.mario)))
+                        mMap!!.moveCamera(CameraUpdateFactory.newLatLngZoom(playerLocation, 14f))
+
+                    }
+                    Thread.sleep(1000)
+                }catch (ex:Exception){
+
+                }
+            }
+
+
+        }
     }
 }
